fa3808687f42dbfbe666fc936e2e44736b3d6aec
Richie Yi <helloworldrichie@gmail.com>
2017-11-26 21:33:35 -0800

    Create contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="contact-section">
+            <p>EMAIL: FOCUSLA@YAHOO.COM</p>
+            <p>TEL: 323.234.2030</p>
+            <p>FAX: 323.234.2026</p>
+        </div>
+    );
+};
+
+export default Contact;
