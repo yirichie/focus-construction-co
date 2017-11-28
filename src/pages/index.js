4797f5cbddf9dcf98811ae216aaf4d64325d9a73
Richie Yi <helloworldrichie@gmail.com>
2017-11-27 18:56:15 -0800

    Fix up home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const IndexPage = () => <div />;
+const IndexPage = () => (
+    <div className="main-section">
+        <h4 className="main-text">FOCUS CONSTRUCTION CO.</h4>
+        <h5>What We Do</h5>
+        <div className="main-icon-section">
+            <div className="main-column">
+                <i className="material-icons medium">build</i>
+                <h6>Build</h6>
+            </div>
+            <div className="main-column">
+                <i className="material-icons medium">create</i>
+                <h6>Design</h6>
+            </div>
+            <div className="main-column">
+                <i className="material-icons medium">
+                    sentiment_very_satisfied
+                </i>
+                <h6>Accomplish</h6>
+            </div>
+        </div>
+    </div>
+);
 
 export default IndexPage;
