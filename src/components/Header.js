effc7d3ec938084e342fa7c7b44ca93a8f3ec54e
Richie Yi <helloworldrichie@gmail.com>
2017-11-27 22:53:14 -0800

    Add subtle changes to nav links and improve responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,24 @@ import img from '../images/logo.png';
 const Header = () => {
     return (
         <div className="header">
-            <div>
-                <Link to="/about">ABOUT</Link>
-                <Link to="/contact">CONTACT</Link>
+            <div className="header-nav nav-left">
+                <Link to="/about" activeStyle={{ color: 'orange' }}>
+                    ABOUT
+                </Link>
+                <Link to="/contact" activeStyle={{ color: 'orange' }}>
+                    CONTACT
+                </Link>
             </div>
-            <Link to="/">
+            <Link to="/" activeStyle={{ color: 'orange' }}>
                 <img src={img} className="logo" />
             </Link>
-            <div>
-                <Link to="/clients">CLIENTS</Link>
-                <Link to="/portfolio">PORTFOLIO</Link>
+            <div className="header-nav nav-right">
+                <Link to="/clients" activeStyle={{ color: 'orange' }}>
+                    CLIENTS
+                </Link>
+                <Link to="/portfolio" activeStyle={{ color: 'orange' }}>
+                    PORTFOLIO
+                </Link>
             </div>
         </div>
     );
